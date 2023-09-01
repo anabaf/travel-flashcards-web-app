468e0fd03541083f35b8bceca159a9d647e36544
anabaf <anabe@LAPTOP-ACNHCF6B.localdomain>
2023-09-01 15:00:36 +0100

    testing github secrets

--- services/homepage-services.js
+++ services/homepage-services.js
@@ -1,4 +1,4 @@
-const API_KEY = config.MY_KEY;
+const API_KEY = process.env.API_KEY;
 console.log(API_KEY);
 const API_URL = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`
 
